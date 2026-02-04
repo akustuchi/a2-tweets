1fc5a7ef935a4f3a8c0b00f27f0505f86642cff2
Anoki Tran <anokit@uci.edu>
2026-02-04 15:12:52 -0800

    Implemented Tweet Dates logic

--- js/about.js
+++ js/about.js
@@ -12,6 +12,21 @@ function parseTweets(runkeeper_tweets) {
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
 	document.getElementById('numberTweets').innerText = tweet_array.length;	
+
+	//Get first tweet and format the date
+	let firstTweet = tweet_array[0];
+	let firstDateString = firstTweet.time.toLocaleDateString("en-US", {weekday: "long", month: "long", day: "numeric", year: "numeric"});
+
+	//Update HTML
+	document.querySelector("#firstDate").innerText = firstDateString;
+
+	//Same thing for last tweet
+	let lastTweet = tweet_array[tweet_array.length - 1];
+	let lastDateString = lastTweet.time.toLocaleDateString("en-US", {weekday: "long", month: "long", day: "numeric", year: "numeric"});
+
+	document.querySelector("#lastDate").innerText = lastDateString;
+
+
 }
 
 //Wait for the DOM to load
